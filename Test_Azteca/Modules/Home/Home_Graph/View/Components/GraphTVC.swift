6a791e3d472d67a59d9bf2ae57a78043c92fb7f7
Mauricio <maurogrov@gmail.com>
2022-11-01 13:13:11 -0600

    implementation Graph with pods

--- Test_Azteca/Modules/Home/Home_Graph/View/Components/GraphTVC.swift
+++ Test_Azteca/Modules/Home/Home_Graph/View/Components/GraphTVC.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import Charts
 
 class GraphTVC: UITableViewCell {
     
     static let id : String = "GraphTVC"
     
     @IBOutlet weak var questionLbl: UILabel!
+    @IBOutlet weak var chartView: PieChartView!
+    
+    var colorsChartView : [NSUIColor] = [
+        NSUIColor(red: 77, green: 180, blue: 175),
+        NSUIColor(red: 255, green: 70, blue: 90),
+        NSUIColor(red: 255, green: 144, blue: 0),
+        NSUIColor(red: 125, green: 133, blue: 0),
+        NSUIColor(red: 248, green: 220, blue: 0),
+        NSUIColor(red: 10, green: 200, blue: 245),
+    ]
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,23 +38,53 @@ class GraphTVC: UITableViewCell {
     func setup(with data: GraphicModel){
         
         questionLbl.text = data.pregunta
+
+        setupChartView()
+        let dataSet = setupEntriesValues(with: data)
+        chartView.data = PieChartData(dataSet: dataSet)
         
-//        cardView.layer.cornerRadius = 20
-//        cardView.layer.borderWidth = 3
-//        cardView.layer.borderColor = UIColor.black.cgColor
-//        cardView.layer.masksToBounds = true
+    }
+    
+    func setupChartView() {
+        chartView.delegate = self
+        chartView.chartDescription.enabled = false
+        chartView.drawHoleEnabled = true
+        chartView.rotationAngle = 0
+        chartView.rotationEnabled = false
+        chartView.isUserInteractionEnabled = false
+        chartView.drawEntryLabelsEnabled = false
+    }
+    
+    func setupEntriesValues(with data: GraphicModel) -> PieChartDataSet {
+        var entries : [PieChartDataEntry] = Array()
+        var colors : [NSUIColor] = []
         
-//        nameLbl.layer.backgroundColor = UIColor.white.cgColor
-//        nameLbl.layer.cornerRadius = 15
-//        nameLbl.layer.borderWidth = 1
-//        nameLbl.layer.borderColor = UIColor.gray.cgColor
+        var indexcolors = 0;
         
-//        imageView.loadGradient(colors: [.gray, .white, .white])
+        print(colorsChartView.count)
+        for i in 0..<data.values.count {
+            let obj = data.values[i]
+            
+            entries.append(PieChartDataEntry(value: Double(obj.value), label: "\(obj.label) \(obj.value)%"))
+            colors.append(colorsChartView[indexcolors])
+            
+            if indexcolors == colorsChartView.count - 1 {
+                indexcolors = 0
+            }
+            else {
+                indexcolors += 1
+            }
+        }
         
-//        nameLbl?.text = data.name.capitalized
+        let dataSet = PieChartDataSet(entries: entries, label: "")
+        dataSet.colors = colors
+        dataSet.drawValuesEnabled = false
         
-        
-      
+        return dataSet
     }
 
+}
+
+extension GraphTVC: ChartViewDelegate {
+    
 }
